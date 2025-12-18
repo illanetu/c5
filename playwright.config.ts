fa787d528ca8b95e35388dc277bff92d30203c0c
illanetu <illanetu@gmail.com>
2025-12-18 21:40:48 +1000

    калькулятор+тесты

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,19 +4,15 @@ import path from 'path';
 export default defineConfig({
   testDir: './tests',
   use: {
-    baseURL: 'file:///',
     headless: true,
   },
-  // Один проект (Chromium) для простоты
   projects: [
     {
       name: 'chromium',
       use: { browserName: 'chromium' },
     },
   ],
-  // Указываем рабочую директорию, чтобы удобнее формировать путь к HTML
-  retries: 0,
-  reporter: 'list',
 });
+
 
 
